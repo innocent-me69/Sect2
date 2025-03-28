5526f0790c4c13d118d1a9fd38ef95863b0b4fda
Aaru <aryarai1569@protonmail.com>
2025-03-28 21:31:22 +0530

    __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,21 +1,28 @@
 from .client import MangaClient, MangaCard, MangaChapter
 from .manhuako import ManhuaKoClient
-from .tmo import TMOClient
-from .mangadex import MangaDexClient
 from .mangasin import MangasInClient
-from .mcreader import MgekoClient
 from .mangakakalot import MangaKakalotClient
 from .manganelo import ManganeloClient
 from .manganato import ManganatoClient
 from .kissmanga import KissMangaClient
 from .mangatigre import MangatigreClient
 from .mangahasu import MangaHasuClient
-from .mangabuddy import MangaBuddyClient
-from .asurascans import AsuraScansClient
 from .ninemanga import NineMangaClient
 from .likemanga import LikeMangaClient
 from .mangasee import MangaSeeClient
-from .mangadistrict import MangaDistrictClient
-from .manga18fx import Manga18fxClient
-from .omgeascans import OmgeaScansClient
+# Not Working for render/koyeb [ONLY VPS]
+from .mgecko import MgekoClient
+from .asurascans import AsuraScansClient
+#Manhwa working clients
+from .mangadex import MangaDexClient
+from .manhwaclan import ManhwaClanClient
+from .comick import ComickClient
+from .mangabuddy import MangaBuddyClient
 from .reaperscans import ReaperScansClient
+from .weebcentral import WeebCentralClient
+from .flamecomics import FlameComicsClient
+#18+ clients
+from .omgeascans import OmgeaScansClient
+from .manga18fx import Manga18fxClient
+from .manhwa18cc import Manhwa18ccClient
+from .mangadistrict import MangaDistrictClient
