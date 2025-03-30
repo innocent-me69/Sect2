7f51075227a45715f2ac358d0d1be6e14a0111bd
Innocent me <thakareankit46@gmail.com>
2025-03-30 20:46:29 +0530

    Update __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -20,7 +20,7 @@ from .manhwaclan import ManhwaClanClient
 from .comick import ComickClient
 from .mangabuddy import MangaBuddyClient
 from .reaperscans import ReaperScansClient
-from .mangasee import MangaSeeClient
+from .mangasin import MangasInClient
 #18+ clients
 from .omgeascans import OmgeaScansClient
 from .manga18fx import Manga18fxClient
