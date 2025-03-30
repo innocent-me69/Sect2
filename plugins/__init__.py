fc770a7b6957f02e183896b85837e4c80ffaca76
Aaru <aryarai1569@protonmail.com>
2025-03-30 22:25:20 +0530

    __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -20,7 +20,7 @@ from .manhwaclan import ManhwaClanClient
 from .comick import ComickClient
 from .mangabuddy import MangaBuddyClient
 from .reaperscans import ReaperScansClient
-from .mangamob import MangaMob
+from .mangamob import mangamob
 #18+ clients
 from .omgeascans import OmgeaScansClient
 from .manga18fx import Manga18fxClient
