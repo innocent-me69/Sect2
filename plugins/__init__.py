c39a69b0fa136cd52dc12ca00bfb8611337034f3
Aaru <aryarai1569@protonmail.com>
2025-04-03 20:50:16 +0530

    __init__.py

--- plugins/__init__.py
+++ plugins/__init__.py
@@ -1,19 +1,9 @@
 from .client import MangaClient, MangaCard, MangaChapter
-from .manhuako import ManhuaKoClient
-from .mangasin import MangasInClient
-from .mangakakalot import MangaKakalotClient
-from .manganelo import ManganeloClient
-from .manganato import ManganatoClient
-from .kissmanga import KissMangaClient
-from .mangatigre import MangatigreClient
-from .mangahasu import MangaHasuClient
-from .ninemanga import NineMangaClient
-from .likemanga import LikeMangaClient
-from .mangasee import MangaSeeClient
-#from .weebcentral import WeebCentralClient
+
 # Not Working for render/koyeb [ONLY VPS]
 from .mgecko import MgekoClient
 from .asurascans import AsuraScansClient
+
 #Manhwa working clients
 from .mangadex import MangaDexClient
 from .manhwaclan import ManhwaClanClient
@@ -21,6 +11,9 @@ from .comick import ComickClient
 from .mangabuddy import MangaBuddyClient
 from .reaperscans import ReaperScansClient
 from .mangamob import mangamob
+from .manhuafast import ManhuaFastClient
+#from .weebcentral import WeebCentralClient
+
 #18+ clients
 from .omgeascans import OmgeaScansClient
 from .manga18fx import Manga18fxClient
